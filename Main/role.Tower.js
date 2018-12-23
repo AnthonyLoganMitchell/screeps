182ecf6993aed496272d7317d4b531890075e897
Anthony <logan@atomicorp.com>
2018-12-22 21:56:21 -0500

    overhaul for tower room defence. Might work, might not.

--- Main/role.Tower.js
+++ Main/role.Tower.js
@@ -16,23 +16,27 @@ Room.prototype.runTower = function(tower) {
       }
     }
   }
+
   if (this.roomStorageID != null) {
     if (Game.getObjectById(this.roomStorageID).hits < Game.getObjectById(this.roomStorageID).hitsMax) {
       repTarget.push(Game.getObjectById(this.roomStorageID));
     }
   }
-  if (this.memory.enemyCreepsIDs != null) {
-    for (let i = 0; i < this.memory.enemyCreepsIDs.length; i++) {
-      if (Game.getObjectById(this.memory.enemyCreepsIDs[i]) != null) {
-        hostileCreeps.push(Game.getObjectById(this.memory.enemyCreepsIDs[i]));
+  if (this.memory.roomEnemyCreepsIDs != null) {
+    for (let i = 0; i < this.memory.roomEnemyCreepsIDs.length; i++) {
+      if (Game.getObjectById(this.memory.roomEnemyCreepsIDs[i]) != null) {
+        hostileCreeps.push(Game.getObjectById(this.memory.roomEnemyCreepsIDs[i]));
       }
     }
   }
-  var closestDamagedStructure = tower.pos.findClosestByRange(repTarget);
+
   var closestHostile = tower.pos.findClosestByRange(hostileCreeps);
   if (closestHostile != null) {
-    tower.attack(closestHostile);
-  } else if (closestDamagedStructure != null) {
-    tower.repair(closestDamagedStructure);
+    tower.attack(tower.pos.findClosestByRange(closestHostile));
+  } else {
+    var closestDamagedStructure = tower.pos.findClosestByRange(repTarget);
+    if (closestDamagedStructure != null) {
+      tower.repair(closestDamagedStructure);
+    }
   }
 } ////
